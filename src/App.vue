1788adabb30d1a880592b57109aaf04f103b4894
HanSolo23 <vanotaran@gmail.com>
2020-11-16 01:54:51 +0300

    add change for isDelete to closeModalDeleteWindow()

--- src/App.vue
+++ src/App.vue
@@ -162,6 +162,7 @@ export default {
     },
     closeModalDeleteWindow() {
       this.isModalDeleteVisible = false;
+      this.isDelete = false;
     },
     showModalEditWindow() {
       this.isModalEditVisible = true;
